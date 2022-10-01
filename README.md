5b95edd530fae305e52fc0046ca98c19b1a35e51
Chutian Wang <chutian_wang@ucsb.edu>
2022-10-01 14:04:18 -0700

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Phys_20AL_tools
 A set of tools for UCSB Physics labs.
+This toolchain is designed to be lightweight yet powerfull to help you compute and plot.
+Python and the module matplotlib is required.
---
+++ RMSD_tool.py
@@ -0,0 +1,20 @@
+import math
+import matplotlib.pyplot as plt
+
+print("Using RMSD = sqrt( sum( (xi - xbar)^2 ) / (N - 1) )")
+inputstr = input("Input data below, separated by a space.\n")
+try:
+    dataset = [float(x) for x in inputstr.split()]
+    if len(dataset) == 1:
+        print("Dataset size = 1, abort.")
+        exit(0)
+except:
+    print("An error occured. Check your input.")
+    exit(0)
+xbar = sum(dataset) / len(dataset)
+rmsd = math.sqrt(sum([(x - xbar) ** 2 for x in dataset]) / (len(dataset) - 1))
+print(f"\nDataset avg. {xbar}")
+print(f"RMSD         {rmsd}")
+print("Dataset------------------------")
+for x in enumerate(dataset):
+    print(f"{x[0]}            {x[1]}")
